46417f2e5aec2122690c4b530467155fbd62359f
Gustavo de Medeiros <gustavo.10.dmc@gmail.com>
2022-07-25 09:49:49 -0300

    Add Point2D.kt implementation

--- app/src/main/kotlin/galaxyraiders/core/physics/Point2D.kt
+++ app/src/main/kotlin/galaxyraiders/core/physics/Point2D.kt
@@ -1,13 +1,15 @@
 @file:Suppress("UNUSED_PARAMETER") // <- REMOVE
 package galaxyraiders.core.physics
 
+import kotlin.math.abs
+
 data class Point2D(val x: Double, val y: Double) {
   operator fun plus(p: Point2D): Point2D {
-    return INVALID_POINT
+    return Point2D(x + p.x, y + p.y)
   }
 
   operator fun plus(v: Vector2D): Point2D {
-    return INVALID_POINT
+    return Point2D(x + v.dx, y + v.dy)
   }
 
   override fun toString(): String {
@@ -15,26 +17,26 @@ data class Point2D(val x: Double, val y: Double) {
   }
 
   fun toVector(): Vector2D {
-    return INVALID_VECTOR
+    return Vector2D(x, y)
   }
 
   fun impactVector(p: Point2D): Vector2D {
-    return INVALID_VECTOR
+    return Vector2D(abs(p.x - x), abs(p.y - y))
   }
 
   fun impactDirection(p: Point2D): Vector2D {
-    return INVALID_VECTOR
+    return impactVector(p).unit
   }
 
   fun contactVector(p: Point2D): Vector2D {
-    return INVALID_VECTOR
+    return impactVector(p).normal
   }
 
   fun contactDirection(p: Point2D): Vector2D {
-    return INVALID_VECTOR
+    return impactDirection(p).normal
   }
 
   fun distance(p: Point2D): Double {
-    return INVALID_DOUBLE
+    return Vector2D(this.x - p.x, this.y - p.y).magnitude
   }
 }
